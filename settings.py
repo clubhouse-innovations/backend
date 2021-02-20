bf53bc1d6166a450ec0284e570b6a3f3293e514d
saharhash <saharhashai@gmail.com>
2021-02-20 22:37:29 +0100

    Added frontend url

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 S3_DOCS_BUCKET = 'club-house-innovations'
 DEFAULT_REGION_S3 = 'us-west-2'
 GMAIL_ACCOUNT_PASS = os.environ['GMAIL_ACCOUNT_PASS']
-FRONTEND_URL = 'www.techcrunch.com'
+FRONTEND_URL = 'www.joinclubcord.com'
